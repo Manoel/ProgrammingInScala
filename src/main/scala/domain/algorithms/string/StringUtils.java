c451fc96f2394cb220e8b6c750a76016e31b38e7
manoel <manoel.menezes.jr@gmail.com>
2016-12-27 13:55:48 -0300

    suffix array.

--- src/main/scala/domain/algorithms/string/StringUtils.java
+++ src/main/scala/domain/algorithms/string/StringUtils.java
@@ -1,10 +1,84 @@
 package domain.algorithms.string;
 
+import hackerrank.string.Rank;
+import hackerrank.string.RankComparator;
+
 import java.util.Arrays;
 
 public class StringUtils {
 
 	public static final RankComparator BY_RANK = new RankComparator();
+
+	private static final int MAXSIZE = 200100;
+	private static final int ALPHABET = 128;
+	private static final int[] p = new int[MAXSIZE];
+	private static final int[] c = new int[MAXSIZE];
+	private static final int[] cnt = new int[MAXSIZE];
+	private static final int[] pn = new int[MAXSIZE];
+	private static final int[] cn = new int[MAXSIZE];
+
+	public static int[] getSuffixArray(String s) {
+		for (int i = 0; i < cnt.length; ++i) {
+			cnt[i] = 0;
+		}
+
+		int n = s.length();
+		for (int i = 0; i < n; ++i) {
+			++cnt[s.charAt(i)];
+		}
+		for (int i = 1; i < ALPHABET; ++i) {
+			cnt[i] += cnt[i - 1];
+		}
+		for (int i = 0; i < n; ++i) {
+			p[--cnt[s.charAt(i)]] = i;
+		}
+		int count = 1;
+		c[p[0]] = count - 1;
+		for (int i = 1; i < n; ++i) {
+			if (s.charAt(p[i]) != s.charAt(p[i - 1]))
+				++count;
+			c[p[i]] = count - 1;
+		}
+		for (int h = 0; (1 << h) < n; ++h) {
+			for (int i = 0; i < n; ++i) {
+				pn[i] = p[i] - (1 << h);
+				if (pn[i] < 0)
+					pn[i] += n;
+			}
+
+			for (int i = 0; i < cnt.length; ++i) {
+				cnt[i] = 0;
+			}
+
+			for (int i = 0; i < n; ++i) {
+				++cnt[c[i]];
+			}
+			for (int i = 1; i < count; ++i) {
+				cnt[i] += cnt[i - 1];
+			}
+			for (int i = n - 1; i >= 0; --i) {
+				p[--cnt[c[pn[i]]]] = pn[i];
+			}
+			count = 1;
+			cn[p[0]] = count - 1;
+			for (int i = 1; i < n; ++i) {
+				int pos1 = (p[i] + (1 << h)) % n;
+				int pos2 = (p[i - 1] + (1 << h)) % n;
+				if (c[p[i]] != c[p[i - 1]] || c[pos1] != c[pos2])
+					++count;
+				cn[p[i]] = count - 1;
+			}
+			for (int i = 0; i < n; ++i) {
+				c[i] = cn[i];
+			}
+		}
+		int[] res = new int[n];
+
+		for (int i = 0; i < n; ++i) {
+			res[i] = c[i];
+		}
+		return res;
+	}
 
 	public static Rank[] suffixArray(String s) {
 
--- src/main/scala/domain/algorithms/string/stringfunctioncalculation/Solution.java
+++ src/main/scala/domain/algorithms/string/stringfunctioncalculation/Solution.java
@@ -22,6 +22,8 @@ public class Solution {
 
 		System.out.println(Arrays.toString(lcp));
 
+		System.out.println(area(lcp));
+		
 		System.out.println(getMaxArea(lcp));
 
 		in.close();
@@ -52,10 +54,65 @@ public class Solution {
 			if (max_area < area_with_top)
 				max_area = area_with_top;
 		}
-		
-		if(max_area > n)
+
+		if (max_area > n)
 			return max_area;
 		else
 			return n;
 	}
+
+	public static int area(int[] height) {
+
+		int n = height.length;
+		if (n == 0)
+			return 0;
+
+		Stack<Integer> left = new Stack<Integer>();
+		Stack<Integer> right = new Stack<Integer>();
+
+		int[] width = new int[n];// widths of intervals.
+		Arrays.fill(width, 1);// all intervals should at least be 1 unit wide.
+
+		for (int i = 0; i < n; i++) {
+			// count # of consecutive higher bars on the left of the (i+1)th bar
+			while (!left.isEmpty() && height[i] <= height[left.peek()]) {
+				// while there are bars stored in the stack, we check the bar on
+				// the top of the stack.
+				left.pop();
+			}
+
+			if (left.isEmpty()) {
+				// all elements on the left are larger than height[i].
+				width[i] += i;
+			} else {
+				// bar[left.peek()] is the closest shorter bar.
+				width[i] += i - left.peek() - 1;
+			}
+			left.push(i);
+		}
+
+		for (int i = n - 1; i >= 0; i--) {
+
+			while (!right.isEmpty() && height[i] <= height[right.peek()]) {
+				right.pop();
+			}
+
+			if (right.isEmpty()) {
+				// all elements to the right are larger than height[i]
+				width[i] += n - 1 - i;
+			} else {
+				width[i] += right.peek() - i - 1;
+			}
+			right.push(i);
+		}
+
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			// find the maximum value of all rectangle areas.
+			max = Math.max(max, width[i] * height[i]);
+		}
+		
+		return max > n ? max : n;
+	}
+
 }
